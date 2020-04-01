2de397eda95ec77a4f69d10381276f9946efac35
Sandykkd <59678434+Sandykkd@users.noreply.github.com>
2020-04-01 20:43:55 +0530

    mikuuuu

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,33 +1,44 @@
-(function(){
-    'use strict';
-    angular.module("LunchCheck",[])
-    .controller('LunchCheckController',function($scope)
-    {
-        
-       
-        $scope.dispNumeric = function() {
-            var tnameval = calcNumForString($scope.name);
-            $scope.t=tnameval;
-           
-        }
-        
-    });
-    function calcNumForString(string){
-        var item = string.split(",");
-                var tlen = item.length;
-                var len = tlen;
-                var res = "";
-                for(var i=0;i<tlen;i++)
-                    if(item[i]=="")
-                        len-=1;
-                if(len==0)
-                    res = "Please enter data first";
-                else if(len<=3)
-                    res = "Enjoy!";
+(
+    function(){
+        angular.module('LunchCheck',[])
+        .controller('LunchCheckController',mycontrol);
+
+        mycontrol.$inject = ['$scope'];
+        function mycontrol($scope){
+            $scope.n="";
+            $scope.val = "";
+            $scope.menu= function(){
+                $scope.val = menu_check($scope.n);
+            }
+
+            function menu_check(string){
+                var str = string.split(",");
+                var arr = str.length;
+                  
+                for(var i=0;i<string.length;i++)   
+                {
+                    if(str[i] == "")
+                        arr=  arr-1;
+                }
+                var txt = "";
+                if(arr==0)
+                
+                    {
+                        txt = "Please enter data first";
+                     }
+                else if(arr<=3)
+                {
+                    txt = "Enjoy!";
+                }
+                
                 else
-                    res = "Too Much!";
-                return res;
-        
+                {
+                    txt = "Too Much!";
+
+                 }
+                    
+                return txt;
+            }
+        };
     }
- 
-})();
+)();
